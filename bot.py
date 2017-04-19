61cf5a6654a8f06d4e6191839bfc78522dd4fd80
nisimatteo834 <nisimatteo834@gmail.com>
2017-04-19 17:34:52 +0200

    First version of MySQL database

--- bot.py
+++ bot.py
@@ -1,20 +1,20 @@
 from __future__ import print_function
-from database import Packet
 import requests
 import json
-import datetime
-import cherrypy
-import paho.mqtt.publish as publish
 import time
 import telepot
-import asynchat
 from database import Packet
-import threading
+from thingspeak import Truck
 
-def retrievePosition():
-    pos = json.loads(requests.get('https://api.thingspeak.com/channels/252276/feeds/last').content)
 
+def retrievePosition(id):
+    p = Packet()
+    truckid = Packet.findTruckAssociation(p,id)
+    channel = Truck.channelIDretrieve(Truck(),truckid)
+    url = 'https://api.thingspeak.com/channels/'+str(channel)+'/feeds/last'
+    pos = json.loads(requests.get(url).content)
     stringa = '{"lat" :' + str(pos['field3']) + ',"long": ' + str(pos['field4']) +'}'
+
 
     return stringa
 
@@ -31,51 +31,45 @@ def on_message(msg):
     if lena != 0:
         id = msg['chat']['id']
 
-
         try:
-            if (any(msg['entities'])):
-                if(msg['entities'][0]['type']== 'bot_command'):
-
-                    bot.sendMessage(id,'Enter your pack code:')
-
-                    time.sleep(10)
-                    updates = bot.getUpdates()
-
-                    if len(updates) == 0:
-                        bot.sendMessage(id,'Session expired. Please try again')
-                        return
-
-                    else:
-                        bot.sendMessage(id,'Received' + updates)
-
-
-
-
-                    if msg['text'] == '/getposition':
-                        try:
-                            po = retrievePosition()
-                            print (po)
-                            pos = json.loads(po)
-                            bot.sendLocation(id,pos['lat'],pos['long'])
-                            return
-
-                        except Exception as detail:
-                            print (detail)
-
-                    elif msg['text'] == "/gettemperature":
-                        return
-
-                    elif msg['text'] == "/gethumidity":
-
-                        return
-
-                    elif msg['text'] == "/getall":
-                        return
-
+            any(msg['entities'])
 
         except:
-            bot.sendMessage(id,'You should send me a /command')
+            bot.sendMessage(id,'You should send me a command')
+            return
+        if(msg['entities'][0]['type']== 'bot_command'):
+
+            if msg['text'] == '/getposition':
+
+
+                bot.sendMessage(id,"Enter yuor pack code:")
+                time.sleep(10)
+                updates = bot.getUpdates()
+
+                packet = (updates[len(updates)-1]['message']['text'])
+
+                try:
+                    po = retrievePosition(str(packet))
+
+                except Exception as detail:
+                    bot.sendMessage("Error in accessing the database")
+                    return
+
+                pos = json.loads(po)
+                bot.sendLocation(id, pos['lat'], pos['long'])
+                return
+
+            elif msg['text'] == "/gettemperature":
+                return
+
+            elif msg['text'] == "/gethumidity":
+                return
+
+            elif msg['text'] == "/getall":
+                return
+
 
 if __name__ == '__main__':
     bot = telepot.Bot('378511160:AAF8PCogZt5ZtPUp_gaJU2BPMoWnF6-8zuQ')
     bot.message_loop({'chat':on_message},relax=20,run_forever=True)
+
